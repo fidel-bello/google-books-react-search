303abb76aea2f456ee327e0681ca325e0ece5964
fidel bello <59423@bears.smcc.edu>
2021-05-14 12:17:12 -0400

    added jumbotron for styling

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import {BrowserRouter as Router, Route } from 'react-router-dom';
 import NavBar from './components/NavBar';
+import Header from './components/Header';
 
 class App extends Component {
   render() {
@@ -8,6 +9,7 @@ class App extends Component {
       <Router>
         <div>
           <NavBar />
+          <Header />
         </div>
       </Router>
     )
