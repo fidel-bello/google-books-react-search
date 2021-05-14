cae2b54d33c8b6cd9007d294b0d3f28e5f544970
fidel bello <59423@bears.smcc.edu>
2021-05-13 22:23:13 -0400

    connected to database

--- server.js
+++ server.js
@@ -1,1 +1,20 @@
-server
+const express = require('express');
+const mongoose = require('mongoose');
+
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"))
+}
+
+mongoose.connect(process.env.MONGOD_URI || "mongodb://localhost/googlebooks", { useNewUrlParser: true});
+
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}`);
+});
