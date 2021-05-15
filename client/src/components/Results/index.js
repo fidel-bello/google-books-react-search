e1c0e904395bdc31f6c1417bf36282512a38099a
fidel bello <59423@bears.smcc.edu>
2021-05-15 09:51:45 -0400

    calling api in setstate

--- client/src/components/Results/index.js
+++ client/src/components/Results/index.js
@@ -1,2 +1,13 @@
-import React from 'react';
+import React, { Component } from 'react';
+import API from '../../utils/API';
 
+class Results extends Component {
+    state = {
+        savedBooks: [],
+    }
+    componentDidMount() {
+        API.savedBooks()
+        .then(savedBooks => this.setState({ savedBooks: savedBooks }))
+        .catch(err => console.log(err));
+    }
+}
