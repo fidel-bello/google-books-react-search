f9a5a127fb8e15d3ec36ded8c7341f66f1555501
fidel bello <59423@bears.smcc.edu>
2021-05-15 10:26:42 -0400

    added new pages to render

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,8 @@ import {BrowserRouter as Router, Route } from 'react-router-dom';
 import NavBar from './components/NavBar';
 import Header from './components/Header';
 import Wrapper from './components/Wrapper';
+import Search from "./pages/Search";
+import Saved from "./pages/Saved";
 
 class App extends Component {
   render() {
@@ -12,7 +14,9 @@ class App extends Component {
           <NavBar />
           <Header />
           <Wrapper>
-            
+          <Route exact path="/" component={Search} />
+            <Route exact path="/search" component={Search} />
+            <Route exact path="/saved" component={Saved} />
           </Wrapper>
         </div>
       </Router>
