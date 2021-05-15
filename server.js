892d1c4330e7b8f7b65d1ed22c5af42e8860c4ba
fidel bello <59423@bears.smcc.edu>
2021-05-15 12:18:15 -0400

    setting up server

--- server.js
+++ server.js
@@ -1,20 +1,32 @@
-const express = require('express');
+const express = require ('express');
 const mongoose = require('mongoose');
-
+const path = require('path');
+const routes = require('./routes');
+require('dotenv').config();
 
 const PORT = process.env.PORT || 3001;
-
 const app = express();
 
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
 
-if (process.env.NODE_ENV === "production") {
-    app.use(express.static("client/build"))
+if(process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
 }
 
-mongoose.connect(process.env.MONGOD_URI || "mongodb://localhost/googlebooks", { useNewUrlParser: true});
+mongoose.connect(process.env.MONGOD_URI || "mongodb://localhost/google-book-search", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+});
+
+app.use(routes);
+
+app.get("*", (req,res) => {
+    res.sendFile(path.join(__dirname, "client/public/index.html"))
+});
 
 app.listen(PORT, () => {
-    console.log(`API server now on port ${PORT}`);
+    console.log(`API server now on port ${PORT}`)
 });
