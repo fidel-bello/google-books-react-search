08cb62f3ebcff11b273eafe77707e75b974bd769
fidel bello <59423@bears.smcc.edu>
2021-05-16 23:00:37 -0400

    get books function

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -5,10 +5,39 @@ import './style.css'
 import Card from '../components/Card';
 import Form from '../components/Form';
 import { List } from '../components/List';
-import Book from '../components/Book'
+import Book from '../components/Book';
+import API from '../utils/API';
 
 // Search Books class component
 class SearchBooks extends Component {
+    state = {
+        books: [],
+        q: '',
+        message: 'Search for book!',
+    };
+
+    handleInputChange = (e) => {
+        const { name, value } = event.target;
+        this.setState({
+            [name]: value,
+        });
+    };
+
+    getBooks = () => {
+        API.getBooks(this.state.q)
+            .then((res) =>
+                this.setState({
+                    books: res.data,
+                })
+            )
+            .catch(() =>
+                this.setState({
+                    books: [],
+                    message: 'No Books Found!',
+                })
+            );
+    };
+
     render() {
         return (
             <>
@@ -23,47 +52,47 @@ class SearchBooks extends Component {
                                 <Jumbotron>
                                     <h1 className="text-center">
                                         <strong style={{ color: 'white' }}>
-                                        Google Books Search
+                                            Google Books Search
 										</strong>
                                     </h1>
                                 </Jumbotron>
                             </Col>
                             <Col size="md-12">
-                                {}
+                                { }
                                 <Card
-									title=" Book Search"
-									icon="far fa-book"
-								>
-                                   <Form /> 
+                                    title=" Book Search"
+                                    icon="far fa-book"
+                                >
+                                    <Form />
                                 </Card>
                             </Col>
                         </Row>
                         <Row>
                             <Col size="md-12">
-                                {}
+                                { }
                                 <Card>
-                                <List>
-											{this.state.books.map(
-												(book) => (
-													<Book
-														key={ book.id }
-														title={	book.volumeInfo.title }
-														subtitle={ book.volumeInfo.subtitle }
-														link={ book.volumeInfo.infoLink }
-														authors={book.volumeInfo.authors.join(', ')}
-														description={ book.volumeInfo.description }
-														image={ book.volumeInfo.imageLinks.thumbnail }
-														Button={() => (
-															<button
-																onClick={() => this.handleBookSave( book.id )}
-																className="btn btn-primary ml-2" >
-																Save
-															</button>
-														)}
-													/>
-												)
-											)}
-										</List>
+                                    <List>
+                                        {this.state.books.map(
+                                            (book) => (
+                                                <Book
+                                                    key={book.id}
+                                                    title={book.volumeInfo.title}
+                                                    subtitle={book.volumeInfo.subtitle}
+                                                    link={book.volumeInfo.infoLink}
+                                                    authors={book.volumeInfo.authors.join(', ')}
+                                                    description={book.volumeInfo.description}
+                                                    image={book.volumeInfo.imageLinks.thumbnail}
+                                                    Button={() => (
+                                                        <button
+                                                            onClick={() => this.handleBookSave(book.id)}
+                                                            className="btn btn-primary ml-2" >
+                                                            Save
+                                                        </button>
+                                                    )}
+                                                />
+                                            )
+                                        )}
+                                    </List>
                                 </Card>
                             </Col>
                         </Row>
