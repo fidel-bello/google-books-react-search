322d6585142cd9db963aa36270dc74fb0a5461e7
fidel bello <59423@bears.smcc.edu>
2021-05-16 23:48:31 -0400

    added saved books page to the app

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import NavBar from './components/NavBar';
+import SavedBooks from './pages/Saved';
 import SearchBooks from './pages/SearchBooks';
 
 function App() {
@@ -11,6 +12,9 @@ function App() {
                 <Switch>
                     <Route exact path='/'>
                         <SearchBooks />
+                    </Route>
+                    <Route exact path='saved'>
+                        <SavedBooks />
                     </Route>
                 </Switch>
             </div>
