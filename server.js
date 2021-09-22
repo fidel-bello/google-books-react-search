4c9a17128e6f2887dc0f25f02e548c9ad1b59dc9
fidel bello <59423@bears.smcc.edu>
2021-09-22 01:15:05 -0400

    added

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require('express');
-const mongoose = require('mongoose');
 const path = require('path');
 const routes = require('./routes');
 const connectDB = require('./config/db');
+require('dotenv').config();
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT ||3001;
 const app = express();
 
 connectDB();
@@ -13,12 +13,12 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
+  app.use('/', express.static(path.join(__dirname, '/client/build')));
 }
 
 app.use(routes);
 
-app.get('/', (req, res) => res.send('Api runnng'));
+app.get('/', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
 
 app.listen(PORT, () => {
   console.log(`API server now on port ${PORT}!`);
