d52b8e9de76301b6bf5bd9fe31d0b54f3183dc14
fidel bello <59423@bears.smcc.edu>
2021-09-21 22:45:50 -0400

    modifications

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const path = require("path");
-const routes = require("./routes");
+const express = require('express');
+const mongoose = require('mongoose');
+const path = require('path');
+const routes = require('./routes');
 const connectDB = require('./config/db');
-
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -13,16 +12,13 @@ connectDB();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
 }
-
 
 app.use(routes);
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
-});
+app.get('/', (req, res) => res.send('Api runnng'));
 
 app.listen(PORT, () => {
   console.log(`API server now on port ${PORT}!`);
